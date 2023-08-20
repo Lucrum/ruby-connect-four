48083816225b4acd7e8b87ffd9b93b6842b46913
Elton <28992495+Lucrum@users.noreply.github.com>
2023-08-20 14:34:07 -0400

    Added token tests and code

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,5 +4,29 @@
 class ConnectFour
   def initialize
     @board = []
+    @player_one_token = nil
+    @player_two_token = nil
+  end
+
+  def player_tokens
+    puts 'Player one token: '
+    loop do
+      @player_one_token = verify_token(gets.chomp)
+      break if @player_one_token
+
+      puts 'Bad token! Please enter a single character: '
+    end
+
+    puts 'Player two token: '
+    loop do
+      @player_two_token = verify_token(gets.chomp)
+      break if @player_two_token
+
+      puts 'Bad token! Please enter a single character: '
+    end
+  end
+
+  def verify_token(token)
+    token.match(/^.{1}$|^[\u2600-\u26FF]{1}$/) ? token : nil
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,5 +1,60 @@
 # frozen_string_literal: true
 
+require_relative '../lib/connect_four'
+
+# rubocop: disable Metrics/BlockLength
+
 describe ConnectFour do
-  
+  describe '#player_tokens' do
+    subject(:game_tokens) { described_class.new }
+
+    context 'when inputting two valid tokens' do
+      before do
+        valid_token_one = '@'
+        valid_token_two = 'X'
+        allow(game_tokens).to receive(:gets).and_return(valid_token_one, valid_token_two)
+      end
+
+      it 'accepts two valid tokens' do
+        message_player_one = 'Player one token: '
+        message_player_two = 'Player two token: '
+        expect(game_tokens).to receive(:puts).with(message_player_one)
+        expect(game_tokens).to receive(:puts).with(message_player_two)
+        game_tokens.player_tokens
+      end
+    end
+
+    context 'when inputting an invalid token, then valid ones' do
+      before do
+        invalid_token = '!!'
+        valid_token_one = '♡'
+        valid_token_two = 'X'
+        allow(game_tokens).to receive(:gets).and_return(invalid_token, valid_token_one, valid_token_two)
+      end
+
+      it 'rejects the invalid, then accepts both valid tokens' do
+        message_player_one = 'Player one token: '
+        message_player_two = 'Player two token: '
+        error_message = 'Bad token! Please enter a single character: '
+        expect(game_tokens).to receive(:puts).with(error_message)
+        expect(game_tokens).to receive(:puts).with(message_player_one)
+        expect(game_tokens).to receive(:puts).with(message_player_two)
+        game_tokens.player_tokens
+      end
+    end
+  end
+
+  describe '#verify_token' do
+    subject(:game_verify_token) { described_class.new }
+
+    it 'returns a valid token' do
+      valid_token = '@'
+      expect(game_verify_token.verify_token(valid_token)).to eq(valid_token)
+    end
+
+    it 'returns nil on an invalid token' do
+      invalid_token = 'hello'
+      expect(game_verify_token.verify_token(invalid_token)).to be_nil
+    end
+  end
 end
